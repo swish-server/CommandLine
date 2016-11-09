8126e2f6deeb3718c7f88049d254e65f4bdb65ea
Thomas Paul Mann <hi@thomaspaulmann.com>
2016-11-09 19:53:12 +0100

    Add test for simple running of commands

--- Tests/CommandLineTests/CommandLineTests.swift
+++ Tests/CommandLineTests/CommandLineTests.swift
@@ -2,16 +2,28 @@ import XCTest
 @testable import CommandLine
 
 class CommandLineTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual(CommandLine().text, "Hello, World!")
-    }
 
+    func testRunCommand() {
+        let runExpectation = expectation(description: "Run <ls> to see files in current directory.")
+
+        let commandLine = CommandLine()
+        let command = "/usr/bin/env"
+        let arguments = [ "ls" ]
+        let result = commandLine.execute(command, with: arguments)
+
+        switch result {
+        case .output(let output):
+            print(output)
+            runExpectation.fulfill()
+        case .error(let message):
+            print("Error: \(message)")
+        }
+    }
 
     static var allTests : [(String, (CommandLineTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testRunCommand", testRunCommand)
         ]
     }
+
 }
